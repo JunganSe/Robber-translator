853faed26e33d2c8bebb29468342bb81ce469ba4
Johan Ljungberg <jlj.9@hotmail.com>
2021-10-27 22:20:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Robber_translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a phrase to translate or q to quit.");
+                string phrase = Console.ReadLine();
+                if (phrase == "q") { break; }
+
+                string robberPhrase = RobberTranslate.ToRobber(phrase);
+                Console.WriteLine("\nTranslated to robber:\n" + robberPhrase);
+
+                string normalPhrase = RobberTranslate.FromRobber(robberPhrase);
+                Console.WriteLine("\nTranslated back from robber:\n" + normalPhrase);
+
+                Console.WriteLine("\n\n");
+            }
+        }
+    }
+}
---
+++ RobberTranslate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robber_translator
+{
+    static class RobberTranslate
+    {
+        private static readonly string consonants = "bcdfghjklmnpqrstvwxz";
+        internal static string ToRobber(string phrase)
+        {
+            string output = "";
+            phrase = phrase.ToLower();
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                char letter = phrase[i];
+                if (consonants.Contains(letter))
+                {
+                    output += letter + "o" + letter;
+                }
+                else
+                {
+                    output += letter;
+                }
+            }
+
+            return output;
+        }
+
+        internal static string FromRobber(string phrase)
+        {
+            string output = "";
+            phrase = phrase.ToLower();
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if ((i <= phrase.Length - 2)
+                    && (consonants.Contains(phrase[i]))
+                    && (phrase[i + 1] == 'o')
+                    && (phrase[i + 2] == phrase[i]))
+                {
+                    i += 2;
+                }
+                output += phrase[i];
+            }
+
+            return output;
+        }
+    }
+}
